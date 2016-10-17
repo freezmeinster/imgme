ba73047a0fc1c35728dc9d707b1f1335d9c4226a
bram <bramandityo>
2016-10-18 03:54:08 +0700

    split config

--- imgme.py
+++ imgme.py
@@ -2,11 +2,16 @@
 
 import json
 import urllib2
+import os
+
+BASE_DIR=os.path.dirname(__file__)
 
 base_image_url="https://images.joyent.com/images"
 
-index_raw = urllib2.urlopen(base_image_url).read()
+# index_raw = urllib2.urlopen(base_image_url).read()
+
+index_raw = open(os.path.join(BASE_DIR,"index.json")).read()
 
 index = json.loads(index_raw)
 
-print index
+print index[0]
